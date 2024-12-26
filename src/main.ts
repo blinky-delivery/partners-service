9ef14b40a8ac45dd81c5fe7e0e8c9ee2aa938d91
Yassine El Kasmy <yassineelkasmy@gmail.com>
2024-12-26 21:36:02 +0100

    updates

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH"],
+    credentials: true,
+  });
   await app.listen(process.env.SERVER_PORT ?? 3000);
 }
 bootstrap();
