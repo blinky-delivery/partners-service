e8f9c3c7baf0e92f6269788ec6dbbaea7daa2e6d
Yassine El Kasmy <yassineelkasmy@gmail.com>
2025-02-15 15:36:58 +0100

    updastes

--- src/availability/availability.service.ts
+++ src/availability/availability.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { eq } from 'drizzle-orm';
 import { DrizzleService } from 'src/database/drizzle.service';
 import { partnersSchema } from 'src/database/partners.database-schema';
 
 interface UpdateAvailabilityParams {
     siteId: string
     availabilities: {
-        id?: string
         storeSiteId: string
         dayOfWeek: number
         timeRangeIndex: number
@@ -17,6 +17,7 @@ interface UpdateAvailabilityParams {
 
 @Injectable()
 export class AvailabilityService {
+    private readonly logger = new Logger(AvailabilityService.name)
 
     constructor(
         private readonly drizzleService: DrizzleService,
@@ -40,6 +41,36 @@ export class AvailabilityService {
         }
     }
 
+
+    async updateSiteAvailability(params: UpdateAvailabilityParams) {
+        try {
+            await this.drizzleService.partnersDb.transaction(async (tx) => {
+                try {
+                    await tx.delete(partnersSchema.storeAvailability)
+                        .where(eq(partnersSchema.storeAvailability.storeSiteId, params.siteId))
+
+                    for (const availability of params.availabilities) {
+                        await tx.insert(partnersSchema.storeAvailability).values({
+                            storeSiteId: params.siteId,
+                            dayOfWeek: availability.dayOfWeek,
+                            timeRangeIndex: availability.timeRangeIndex,
+                            openTime: availability.openTime,
+                            closTime: availability.closTime,
+                        })
+                    }
+
+                } catch (error) {
+                    tx.rollback()
+                    throw error
+                }
+            })
+
+
+        } catch (error) {
+            this.logger.error(`Error updating site availability: ${error}`)
+            throw new InternalServerErrorException(error)
+        }
+    }
 
 
 }
